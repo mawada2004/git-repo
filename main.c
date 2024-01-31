57bc372065a845eef029bd29ce385fc668e5cb43
mawada ramadan <142642968+mawada2004@users.noreply.github.com>
2024-01-31 14:55:27 +0200

    third file

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    int n;
-    printf("please enter any value: \n");
-    scanf("%d",&n);
-    printf("you entered: %d",n);
+    float n;
+   printf("enter a dollar and cents amount \n");
+   scanf("%f",&n);
+   printf("your money after %5 tax = %.2f",n+(n*0.05));
 
     return 0;
 }
